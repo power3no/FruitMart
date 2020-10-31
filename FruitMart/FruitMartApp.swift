d0429c7e26bc328ff8b13e44f369eb8cf9ee3fdf
GyeongJunLee <rudwns1113@gmail.com>
2020-10-31 15:08:20 +0900

    Initial Commit

---
+++ FruitMart/FruitMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMartApp.swift
+//  FruitMart
+//
+//  Created by Jun on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct FruitMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
