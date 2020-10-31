6de34d996e56a6a2854978a96fc0fd1f9c350118
GyeongJunLee <rudwns1113@gmail.com>
2020-10-31 19:54:17 +0900

    4. 프로퍼티로 뷰 추출하기

--- FruitMart/Source/Home/ProductRow.swift
+++ FruitMart/Source/Home/ProductRow.swift
@@ -10,37 +10,8 @@ import SwiftUI
 struct ProductRow: View {
     var body: some View {
         HStack{
-            Image("apple")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 140)
-                .clipped()
-            VStack(alignment: .leading) {
-                Text("백설공주 사과")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 6)
-                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                Spacer()
-                HStack() {
-                    Text("¥").font(.footnote)
-                        + Text("2100").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "heart")
-                        .imageScale(.large)
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Image(systemName: "cart")
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }
-            }
-            .padding([.leading, .bottom], 12)
-            .padding([.top, .trailing])
+            productImage
+            productDescription
         }
         .frame(height: 150)
         .background(Color.primary.colorInvert())
@@ -48,6 +19,47 @@ struct ProductRow: View {
         .shadow(color: Color.primary.opacity(0.33), radius: 1, x: 2, y: 2)
         .padding(.vertical, 8)
     
+    }
+}
+
+private extension ProductRow {
+    var productImage: some View {
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+    var productDescription: some View {
+        VStack(alignment: .leading) {
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            Spacer()
+            footerView
+        }
+        .padding([.leading, .bottom], 12)
+        .padding([.top, .trailing])
+    }
+    var footerView: some View {
+        HStack() {
+            Text("¥").font(.footnote)
+                + Text("2100").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Image(systemName: "cart")
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
