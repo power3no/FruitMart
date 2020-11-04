78496795512838e0b239caa3102123a1b6a3881a
GyeongJun Lee <rudwns1113@gmail.com>
2020-11-04 12:03:14 +0900

    8. 네비게이션 링크를 통한 화면 전환하기

--- FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/Source/User Interface/Home/Home.swift
@@ -11,8 +11,15 @@ struct Home: View {
     let store: Store
     
     var body: some View {
-        List(store.products) { product in
-            ProductRow(product: product)
+        NavigationView{
+            List(store.products) { product in
+                NavigationLink(destination: Text("상세 정보")) {
+                    ProductRow(product: product)
+                }
+            }
+            
+            
+            .navigationTitle("과일마트")
         }
     }
 }
