643392cbdd7b55c856c34df47eaf1ba82ac91dba
GyeongJunLee <rudwns1113@gmail.com>
2020-11-08 21:34:10 +0900

    26. 이미지 갤러리 구현하기

--- FruitMart/Source/User Interface/ImageGallery/ImageGallery.swift
+++ FruitMart/Source/User Interface/ImageGallery/ImageGallery.swift
@@ -23,7 +23,7 @@ struct ImageGallery: View {
                 frontCard
             }
             Spacer()
-//            controller
+            controller
         }
         .edgesIgnoringSafeArea(.top)
     }
@@ -50,15 +50,55 @@ private extension ImageGallery {
     
     func backgroundCard(image: String) -> some View {
         let index = productImage.firstIndex(of: image)!
+        let response = computeResponse(index: index)
+        let animation = Animation.spring(response: response, dampingFraction: 0.68)
+        
         return FruitCard(image)
             .shadow(color: .primaryShadow, radius: 2, x: 2, y: 2)
             .offset(computePosition(index: index))
+            .scaleEffect(computeScale(index: index))
+            .rotation3DEffect(computeAngle(index: index), axis: (0,0,1))
+            .transition(AnyTransition.scale.animation(animation))
+            
     }
     
     func computePosition(index: Int) -> CGSize {
         let x = translation.width
         let y = translation.height - CGFloat(index) * spacing
         return CGSize(width: x, height: y)
+    }
+    
+    func computeScale(index: Int) -> CGFloat {
+        let cardScale = 1.0 - CGFloat(index) * (0.05 - scale)
+        return max(cardScale, 0.1)
+    }
+    
+    func computeAngle(index: Int) -> Angle {
+        let degrees = Double(index) * Double(angle)
+        return Angle(degrees: degrees)
+    }
+    
+    func computeResponse(index: Int) -> Double {
+        max(Double(index) * 0.04, 0.2)
+    }
+    
+    var controller: some View {
+        let titles = ["간격", "크기", "각도"]
+        let values = [$spacing, $scale, $angle]
+        let ranges: [ClosedRange<CGFloat>] = [1.0...40.0, 0...0.05, -90.0...90.0]
+        
+        return VStack {
+            ForEach(titles.indices) { i in
+                HStack {
+                    Text(titles[i])
+                        .font(.system(size: 17))
+                        .frame(width: 80)
+                    Slider(value: values[i], in: ranges[i])
+                        .accentColor(Color.gray.opacity(0.25))
+                }
+            }
+        }
+        .padding()
     }
 }
 
